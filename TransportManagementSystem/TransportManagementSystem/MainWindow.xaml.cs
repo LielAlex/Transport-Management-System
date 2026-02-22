f941ac80e034546c57db98f6d36c82c72a1f49d3
SCS\lielb <liel.biteghe@scsconcept.com>
2026-02-22 19:53:11 +0100

    Build dashbord

--- TransportManagementSystem/TransportManagementSystem/MainWindow.xaml.cs
+++ TransportManagementSystem/TransportManagementSystem/MainWindow.xaml.cs
@@ -1,28 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace TransportManagementSystem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            MainFrame.Navigate(new Pages.DriversPage());
+        }
+
+        private void BtnDrivers_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Pages.DriversPage());
+        }
+
+        private void BtnCustomers_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Pages.CustomersPage());
+        }
+
+        private void BtnVehicles_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Pages.VehiclesPage());
+        }
+
+        private void BtnBookings_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Pages.BookingsPage());
+        }
+
+        private void BtnDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Pages.DashboardPage());
         }
     }
 }
